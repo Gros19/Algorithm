e57b3cd6dbbd91629d0bce5245e728b827fd4ffd
Gros19 <68102580+Gros19@users.noreply.github.com>
2021-03-30 17:03:08 +0900

    공통 나머지 출력 문제

---
+++ beak3.java
@@ -0,0 +1,21 @@
+package Baekjoon;
+import java.util.Scanner;
+
+public class beak3 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		int first = scn.nextInt();
+		int su[] = new int[first];
+		
+		for(int i = 0; i<first; i++) {
+			su[i] = scn.nextInt();
+		}
+		
+		
+
+
+	}
+
+}
