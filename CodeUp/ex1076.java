a274e762b06618f32d648401b6c33f526f3c4ee6
Gros19 <68102580+Gros19@users.noreply.github.com>
2021-04-01 13:20:40 +0900

    Add files via upload
    코드업

---
+++ CodeUp/ex1076.java
@@ -0,0 +1,61 @@
+
+//import java.util.Scanner;
+//public class Main{
+//    public static void main(String[]args){
+//        
+//					
+//        
+//    }
+//}
+//
+
+/*
+ * 
+ * 
+영문자 1개가 입력된다.
+(a ~ z)
+
+
+출력
+a부터 입력한 문자까지 순서대로 공백을 두고 출력한다.
+
+
+입력 예시   
+f
+//f 111
+//a 97
+ *
+ *
+출력 예시
+a b c d e f
+ */
+
+package CodeUp;
+
+import java.util.Scanner;
+
+public class ex1076 {
+	
+	int velocity;
+	public int horese;
+	
+	
+	
+	void call(int a){
+		
+		
+	}
+	
+	public static void main(String[]args){
+		
+		
+		}
+		
+	
+		}
+		
+		
+		
+
+
+
