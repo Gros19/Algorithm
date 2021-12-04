9057b06c53f6524ec6f2328960db4d835457918a
Gros19 <kjj6393@gmail.com>
2021-12-04 23:49:02 +0900

    1단계, 나눗셈 문제

---
+++ Java/beakjoon/step1/Step1_10430.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Step1_10430 {
+    public static void main(String[] args) {
+        /*
+       첫째 줄에 (A+B)%C, 
+       둘째 줄에 ((A%C) + (B%C))%C, 
+       셋째 줄에 (A×B)%C, 
+       넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+         */
+        Scanner scn = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder(scn.nextLine());
+        String[] ins = sb.toString().split(" ", 3);
+        int A = Integer.parseInt(ins[0]);
+        int B = Integer.parseInt(ins[1]);
+        int C = Integer.parseInt(ins[2]);
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
