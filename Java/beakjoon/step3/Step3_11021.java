20fba27b4a58c412b0831aa8fe2576bd09a5b153
Gros19 <68102580+Gros19@users.noreply.github.com>
2021-12-19 22:57:26 +0900

    Add files via upload
    #num : sum 출력

---
+++ Java/beakjoon/step3/Step3_11021.java
@@ -0,0 +1,45 @@
+package step3;
+
+import java.util.Scanner;
+
+/*
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+
+예제 입력 1
+5
+1 1
+2 3
+3 4
+9 8
+5 2
+예제 출력 1
+Case #1: 2
+Case #2: 5
+Case #3: 7
+Case #4: 17
+Case #5: 7
+ */
+public class Step3_11021 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int N = scn.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1;i <= N; ++i){
+
+            int a = scn.nextInt();
+            int b = scn.nextInt();
+            sb.append("Case #"+i+": "+(a+b)+"\n");
+        }
+
+        System.out.println(sb);
+    }
+}
