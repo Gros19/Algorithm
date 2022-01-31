53ffceca8a758fcb94ec872cd4166c0870adb254
Gros19 <kjj6393@gmail.com>
2022-02-01 07:52:45 +0900

    중앙값 구하기

---
+++ Java/Dstructure/Median.java
@@ -0,0 +1,21 @@
+﻿public class Median {
+    public static int med3(int a, int b, int c){
+        if(a >= b){
+            if(b >= c){
+                return b;
+            }else if(a <= c){
+                return a;
+            }else{
+                return c;
+            }
+        }// b > a
+        else if(a > c){
+            return a;
+        }// a <= c
+        else if (b > c) {
+            return c;
+        }else{
+            return b;
+        }
+    }
+}
