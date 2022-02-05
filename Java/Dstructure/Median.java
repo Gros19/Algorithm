163a764ffd75e1d4ce42a872aaa490f70ade0dbe
Gros19 <kjj6393@gmail.com>
2022-02-05 13:33:13 +0900

    Do it 자료구조와 함께 배우는 알고리즘 입문 ------------------------------ 3 요소 중앙값 ------------------------------ 주석 설정

--- Java/Dstructure/Median.java
+++ Java/Dstructure/Median.java
@@ -1,20 +1,53 @@
-﻿public class Median {
+/*
+Do it
+자료구조와 함께 배우는 알고리즘 입문
+------------------------------
+3 요소 중앙값
+ */
+public class Median {
+    public static void main(String[] args){
+        System.out.println(med3(1, 1, 4));
+    }
     public static int med3(int a, int b, int c){
         if(a >= b){
+            //a >= b
+            //b >= c
             if(b >= c){
+                // a >= b >= c
                 return b;
-            }else if(a <= c){
+            }
+            //a >= b
+            //c > b
+            else if(a <= c){
+                // c >= a >= b
                 return a;
-            }else{
+            }
+            // a >= b
+            // c > b
+            // a > c
+            else{
+                // a > c > b
                 return c;
             }
-        }// b > a
+        }
+        // b > a
+        // a > c
         else if(a > c){
+            //b > a > c
             return a;
-        }// a <= c
+        }
+        // b > a
+        // a <= c
+        // b > c
         else if (b > c) {
+            // b > c >= a
             return c;
-        }else{
+        }
+        // b > a
+        // a <= c
+        // b <= c
+        else{
+            // c >= b > a
             return b;
         }
     }
