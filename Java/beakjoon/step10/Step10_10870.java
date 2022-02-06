8fb382dd1a2adc727712a0543b3f2e726e7b900d
Gros19 <kjj6393@gmail.com>
2022-02-06 13:15:12 +0900

    재귀-피보나치 수 5 ------------------------------ n번째 피보나치 수를 출력 ------------------------------

---
+++ Java/beakjoon/step10/Step10_10870.java
@@ -0,0 +1,56 @@
+package step10;
+
+import java.util.Scanner;
+
+/*
+재귀-피보나치 수 5
+
+문제
+피보나치 수는 0과 1로 시작한다. 0번째 피보나치 수는 0이고, 1번째 피보나치 수는 1이다. 그 다음 2번째 부터는 바로 앞 두 피보나치 수의 합이 된다.
+
+이를 식으로 써보면 Fn = Fn-1 + Fn-2 (n ≥ 2)가 된다.
+
+n=17일때 까지 피보나치 수를 써보면 다음과 같다.
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
+
+n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 n이 주어진다. n은 20보다 작거나 같은 자연수 또는 0이다.
+
+출력
+첫째 줄에 n번째 피보나치 수를 출력한다.
+
+예제 입력 1
+10
+예제 출력 1
+55
+ */
+public class Step10_10870 {
+
+    private static int a = 1, c = 0;
+    public static void main(String[]args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        System.out.println(Fibonacci(n));
+    }
+
+    /*
+    2
+    0, 1, 1
+     */
+    public static int Fibonacci(int n){
+        if(n > 0){
+
+            int tmp = c+a;
+            c = a;
+            a = tmp;
+            n--;
+            Fibonacci(n);
+        }
+        return c;
+    }
+}
+
+
