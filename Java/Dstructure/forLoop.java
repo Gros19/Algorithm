00c1a415ed4b6f43d3db9682d41b369041d299b2
Gros19 <kjj6393@gmail.com>
2022-02-11 17:28:44 +0900

    Do it 자료구조와 함께 배우는 알고리즘 입문 ------------------------------ loop ------------------------------ Q12, Q13 ,Q14, Q15a, Q15b, Q15c, Q15d, 16, 17

--- Java/Dstructure/forLoop.java
+++ Java/Dstructure/forLoop.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.Scanner;
+
 /*
 Do it
 자료구조와 함께 배우는 알고리즘 입문
@@ -8,7 +10,7 @@ loop
 public class forLoop {
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(eightyOne());
+        bw.write(pyramid2());
         bw.flush();
 
         bw.close();
@@ -111,4 +113,245 @@ public class forLoop {
         return s.toString();
 
     }
+
+    //Q13
+    public static String eightyOnePlus(){
+/*
+                    | 1  2  3  4  5  6  7  8  9
+                ----+---------------------------
+                 1  | 2  3  4  5  6  7  8  9 10
+                 2  | 3  4  5  6  7  8  9 10 11
+                 3  | 4  5  6  7  8  9 10 11 12
+                 4  | 5  6  7  8  9 10 11 12 13
+                 5  | 6  7  8  9 10 11 12 13 14
+                 6  | 7  8  9 10 11 12 13 14 15
+                 7  | 8  9 10 11 12 13 14 15 16
+                 8  | 9 10 11 12 13 14 15 16 17
+                 9  |10 11 12 13 14 15 16 17 18
+
+                 Process finished with exit code 0
+
+*/
+        StringBuilder s = new StringBuilder();
+        for(int i = -1; i< 10; i++){
+            int k = i;
+            if(i== -1){
+                k = 1;
+            }
+            for(int j = -1; j < 10; j++){
+                if(i+j < -1){
+                    s.append("   ");
+                }else if(i * j == 0){
+                    if(i+j == 0){
+                        s.append("-+");
+                    }
+                    else if(j==0){
+                        s.append(" |");
+                    }else{
+                        s.append("---");
+                    }
+                }else {
+                    if(i*j < 0){
+                        s.append(" ").append(k).append(" ");
+                        k++;
+                    }else{
+                        s.append(String.format("%2s ", (k - 1) + j));
+                    }
+                }
+
+            }
+            s.append("\n");
+        }
+        return s.toString();
+
+    }
+
+    //Q14
+    public static String rectangle(){
+        Scanner scn = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = scn.nextInt();
+        for(int i = 0; i < n;i ++){
+            sb.append(String.format("%"+n+"s","").replace(" ", "*"));
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    //Q15a
+    public static String triangleA(){
+
+        /*
+
+        5
+        *
+        **
+        ***
+        ****
+        *****
+
+        Process finished with exit code 0
+        */
+
+        Scanner scn = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = scn.nextInt();
+        int i = n;
+        while(i-- > 0){
+            sb.append(String.format("%"+(n-i)+"s","").replace(" ", "*"));
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    //Q15b
+    public static String triangleB(){
+        /*
+        7
+        *******
+        ******
+        *****
+        ****
+        ***
+        **
+        *
+
+        Process finished with exit code 0
+
+                    */
+        Scanner scn = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int i = scn.nextInt();
+
+        while(i > 0){
+            sb.append(String.format("%"+(i)+"s","").replace(" ", "*"));
+            i--;
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    //Q15c
+    public static String triangleC(){
+        /*
+        6
+        ******
+         *****
+          ****
+           ***
+            **
+             *
+
+        Process finished with exit code 0
+
+        */
+        Scanner scn = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = scn.nextInt();
+        int i = n;
+        n = 0;
+        while(i > 0){
+            if(n!=0)
+                sb.append(String.format("%"+n+"s",""));
+            sb.append(String.format("%"+(i)+"s","").replace(" ", "*"));
+            i--;
+            n++;
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    //Q15d
+    public static String triangleD(){
+        /*
+        8
+               *
+              **
+             ***
+            ****
+           *****
+          ******
+         *******
+        ********
+        */
+        Scanner scn = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = scn.nextInt();
+        int i = 1;
+        while(n-- > 0){
+            if(n!=0)
+                sb.append(String.format("%"+n+"s",""));
+            sb.append(String.format("%"+(i)+"s","").replace(" ", "*"));
+            i++;
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    //Q16
+    public static String pyramid(){
+        /*
+
+        4
+           *
+          ***
+         *****
+        *******
+
+        Process finished with exit code 0
+        */
+
+        Scanner scn =new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int n = scn.nextInt();
+        int star = 1;
+        int space = n-1;
+
+        while(n-- > 0){
+            if(space!=0)
+                sb.append(String.format("%"+space+"s", ""));
+            sb.append(String.format("%"+star+"s\n", "").replace(" ", "*"));
+            star+=2;
+            space--;
+//          or (star-1)*2+1
+
+        }
+        return sb.toString();
+    }
+
+    //Q17
+    public static String pyramid2(){
+        /*
+6
+     1
+    222
+   33333
+  4444444
+ 555555555
+66666666666
+
+Process finished with exit code 0
+        */
+
+        Scanner scn =new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int n = scn.nextInt();
+        int star = 1;
+        int space = n-1;
+
+        n++;
+        for(int i = 1; i < n; i++){
+            if(space!=0)
+                sb.append(String.format("%"+space+"s", ""));
+            sb.append(String.format("%"+star+"s\n", "").replace(" ", String.valueOf(i)));
+            star+=2;
+            space--;
+//          or (star-1)*2+1
+
+        }
+        return sb.toString();
+    }
+
 }
