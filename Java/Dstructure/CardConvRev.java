9bd5ee18499cb38e613aff778f4aacfe31b94f6c
Gros19 <kjj6393@gmail.com>
2022-02-15 11:10:16 +0900

    Do it 자료구조와 함께 배우는 알고리즘 입문 ------------------------------ 입력받은 no를 cd진수로 변환 ------------------------------

--- Java/Dstructure/CardConvRev.java
+++ Java/Dstructure/CardConvRev.java
@@ -14,6 +14,28 @@ public class CardConvRev {
         return digits;
     }
 
+    public static int cardConv(int x, int r, char[] d){
+        String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        int digits = 1; // 변환될 기수의 크기
+        int nr = r;
+
+
+
+        while (x >= nr) {
+            digits++;
+            nr*=r;
+        }
+        int size = digits;
+
+        do{
+            d[--digits] = dchar.charAt(x%r); //r로 나눈 나머지를 저장
+            x /= r;
+        }while(x != 0);
+
+        return size;
+    }
+
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
 
@@ -41,12 +63,22 @@ public class CardConvRev {
                 cd = scn.nextInt();
             } while (cd < 2 || cd > 36);
 
+            //역변환
             dno = cardCOnvR(no, cd, cno);
             System.out.print(cd+ "진수로 변환 결과: ");
             for(int i = --dno; i >= 0; i--){
                 System.out.print(cno[i]);
             }
             System.out.println("입니다.");
+
+            StringBuilder res = new StringBuilder();
+
+            //정변환
+            dno = cardConv(no, cd, cno);
+            for(int i = 0; i < dno; i++){
+                res.append(cno[i]);
+            }
+            System.out.printf("%d진수로 변환 결과: %s입니다.",cd, res);
 
             System.out.println("한 번 더 하려면 1, 아니면 0을 눌러주세요.");
             retry = scn.nextInt();
