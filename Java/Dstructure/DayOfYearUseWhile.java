fb1d35b94a354be19af6d392c8686ff458fa0615
Gros19 <kjj6393@gmail.com>
2022-02-19 10:06:21 +0900

    Do it 자료구조와 함께 배우는 알고리즘 입문 ------------------------------ 윤년 구하기 dayOfYear 수정 ------------------------------

---
+++ Java/Dstructure/DayOfYearUseWhile.java
@@ -0,0 +1,48 @@
+package Dstructure;
+
+import java.util.Scanner;
+
+public class DayOfYearUseWhile {
+    static int[][] mdays = {
+            {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},   //평년
+            {31, 29, 31, 31, 31, 30, 31, 31, 30, 31, 30, 31}    //윤년
+    };
+
+    // (윤년:1, 평년:0)
+     static int isLeap(int year){
+         if((year % 4 == 0 && year % 100 != 0 || year % 400 == 0)){
+             return 1;
+         }
+         else{
+             return 0;
+         }
+     }
+
+     static int dayOfYear(int y, int m, int d){
+         while(--m>0){
+             d += mdays[isLeap(y)][(m) - 1];
+         }
+         return d;
+     }
+
+     public static void main(String[] args){
+         Scanner scn  = new Scanner(System.in);
+
+         int retry;
+         do{
+             System.out.println("년: ");
+             int year = scn.nextInt();
+             System.out.println("월: ");
+             int month = scn.nextInt();
+             System.out.println("일: ");
+             int day = scn.nextInt();
+
+             System.out.printf("그 해의 %d일째 날입니다." , dayOfYear(year, month, day));
+
+             System.out.println("한 번 더 할까요?(1: 네, 2: 아니오): ");
+             retry = scn.nextInt();
+         }while(retry == 1);
+     }
+
+
+}
