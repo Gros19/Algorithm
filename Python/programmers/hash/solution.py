1a9db549b1acfdf41fba4ca0e11997c0d5ad72e8
Gros19 <kjj6393@gmail.com>
2022-03-30 23:54:23 +0900

    hash

--- Python/programmers/hash/solution.py
+++ Python/programmers/hash/solution.py
@@ -1,1 +1,27 @@
-####
+def solution(participant, completion):
+    tmp = 0
+    dic = {}
+
+    for p in participant:
+        dic[hash(p)] = p
+        tmp += int(hash(p))
+
+    for com in completion:
+
+        tmp -= hash(com)
+
+    return dic[tmp]
+
+9
+5
+4
+7
+2
+3
+
+2
+3
+4
+5
+9
+3
