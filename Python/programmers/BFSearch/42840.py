625cf2b80d8e34c845da0a43038017983222f009
Gros19 <kjj6393@gmail.com>
2022-04-15 10:55:14 +0900

    완전탐색-모의고사

---
+++ Python/programmers/BFSearch/42840.py
@@ -0,0 +1,31 @@
+def solution(answers):
+    student1 = [1, 2, 3, 4, 5]
+    student2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    
+    results = {1: 0, 2: 0, 3: 0}
+
+    for i in range(len(answers)):
+        if student1[i % 5] == answers[i]:
+            results[1] += 1
+
+        if student2[i % 8] == answers[i]:
+            results[2] += 1
+
+        if student3[i % 10] == answers[i]:
+            results[3] += 1
+
+    top_score = max(results.values())
+
+    answers = []
+    for i in range(1, 4):
+        if results[i] == top_score:
+            answers.append(i)
+
+    return answers
+
+
+a = list([1,3])
+
+print(solution(a))
